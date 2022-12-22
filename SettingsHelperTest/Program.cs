18a9940e12e1ec10b94ec202029feca68e753c45
Tanner.Hollis <hollis.tanner@gmail.com>
2022-12-22 10:43:23 -0500

    Test Verification updates

--- SettingsHelperTest/Program.cs
+++ SettingsHelperTest/Program.cs
@@ -14,8 +14,8 @@ namespace SettingsHelperTest
         {
             // Test SettingsGroup.cs
             {
-                string exampleFile = "Set_G1.txt";
                 bool testPass;
+                string exampleFile = "Set_G1.txt";
 
                 // Open file and write to PHROT
                 SettingsGroup settingGroup1 = new SettingsGroup(exampleFile);
@@ -31,19 +31,34 @@ namespace SettingsHelperTest
 
             // Test SettingsTranslator.cs
             {
+                bool testPass = true;
                 string[] wordBits =
                 {
                     "FREQ","ROT", "PTR", "CTR", "27P1P", "27P1D", "27P2P", "27P2D",
                     "59P1P", "59P1D", "59P2P", "59P2D", "81U1P", "81U1D", "81U2P", "81U2D",
                     "81O1P", "81O1D", "81O2P", "81O2D"
                 };
+
+                string[] translatedWordBits =
+                {
+                    "NFREQ","PRROT", "PTR", "CTR", "27P1P", "SV18PU", "27P2P", "SV19PU",
+                    "59P1P", "SV20PU", "59P2P", "SV21PU", "81D1P", "81D1D", "81D2P", "81D2D",
+                    "81D3P", "81D3D", "81D4P", "81D4D"
+                };
+
+                // Create translator instance of type: SEL 351-7
                 string relayType = "SEL 351-7";
                 SettingsTranslator translator = new SettingsTranslator(relayType);
                 
-                foreach(string wordBit in wordBits)
+                for(int i  = 0; i < wordBits.Length; i++)
                 {
-                    Console.WriteLine(wordBit + " -> " + translator.LookupWordBit(wordBit));
+                    Console.WriteLine(wordBits[i] + " -> " + translator.LookupWordBit(wordBits[i]));
+                    
+                    // Verify translator works
+                    testPass &= translator.LookupWordBit(wordBits[i]).Equals(translatedWordBits[i]);
                 }
+
+                PrintTestPass("SettingsTranslator.cs", testPass);
             }
 
             // Test CalcSheet.cs
